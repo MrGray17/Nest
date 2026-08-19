5fcc5ffc2912320bb6035fe6c023e1a7764fc983
El Yazid Hammoubel <hammoubelyazid@gmail.com>
2026-08-19 15:56:28 +0100

    feat: mount weather and quote ambience

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import QuoteWhisper from "./QuoteWhisper";
+import WeatherOverlay from "./WeatherOverlay";
 import "./styles.css";
+import "./weather.css";
+import "./quotes.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <WeatherOverlay />
+    <QuoteWhisper />
   </StrictMode>,
 );
